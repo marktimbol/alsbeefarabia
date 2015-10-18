fa50d680024028e8b93ea0e8ff6d26d8c3c8df05
Mark Timbol <mark.timbol@hotmail.com>
2015-10-18 16:37:22 +0400

    menu details - WIP

--- resources/views/public/menu-details.blade.php
+++ resources/views/public/menu-details.blade.php
@@ -28,22 +28,31 @@
 		</div>
 	</div>
 
-{{-- 	<hr />
+	<hr />
 	
 	<div class="row">
 
-		<div class="col-md-4">
-			<img src="http://placehold.it/500x140" alt="" title="" class="img-responsive" />
+		<div class="col-md-4 wow fadeInLeft" data-wow-delay="0.2s">
+			
+			<a href="{{ route('menus') }}">
+				{!! getPhoto('discover-our-food.jpg', 'Discover our Food') !!}
+			</a>
+
 		</div>
 
-		<div class="col-md-4">
-			<img src="http://placehold.it/500x140" alt="" title="" class="img-responsive" />
+		<div class="col-md-4 wow fadeInLeft" data-wow-delay="0.4s">
+
+			<a href="{{ route('stores') }}">
+				{!! getPhoto('where-are-we.jpg', 'Our Stores') !!}
+			</a>
 		</div>
 
-		<div class="col-md-4">
-			<img src="http://placehold.it/500x140" alt="" title="" class="img-responsive" />
+		<div class="col-md-4 wow fadeInLeft" data-wow-delay="0.6s">
+			<a href="{{ route('about') }}">
+				{!! getPhoto('who-we-are.jpg', 'About Us') !!}
+			</a>
 		</div>
-	</div>	 --}}
+	</div>	
 
 @endsection
 
