435b3f20fe0d744d20dedc3dee7186a2b2664216
Mark Timbol <mark.timbol@hotmail.com>
2015-10-25 14:57:36 +0400

    Add autoreply in contact us page

--- resources/views/emails/auto-reply.blade.php
+++ resources/views/emails/auto-reply.blade.php
@@ -1,8 +1,16 @@
+<p>
 Thanks for contacting Al's Beef!
+</p>
 
+<p>
 This is just a quick note to let you know we've received your message, and will
 respond as soon as we can.
+</p>
 
+<p>
 Regards,
+</p>
 
+<p>
 Al's Beef
+</p>
