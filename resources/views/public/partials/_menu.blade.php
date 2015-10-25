ab1dc5f5b81978601efd927551919beb51d8eb96
Mark Timbol <mark.timbol@hotmail.com>
2015-10-25 15:16:25 +0400

    Add facebook and twitter link

--- resources/views/public/partials/_menu.blade.php
+++ resources/views/public/partials/_menu.blade.php
@@ -8,7 +8,7 @@
 </nav>
 
 <div class="social-icons">
-	<a href="#"><i class="fa fa-facebook"></i></a>
-	<a href="#"><i class="fa fa-twitter"></i></a>
+	<a href="https://www.facebook.com/alsbeef" target="_blank"><i class="fa fa-facebook"></i></a>
+	<a href="https://twitter.com/alsitalianbeef" target="_blank"><i class="fa fa-twitter"></i></a>
 	<a href="#"><i class="fa fa-instagram"></i></a>
 </div>
