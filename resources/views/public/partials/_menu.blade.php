bb9e8456a03e069665bd169dc814f2229a5b122f
Mark Timbol <mark.timbol@hotmail.com>
2015-10-25 15:19:14 +0400

    Add social media links

--- resources/views/public/partials/_menu.blade.php
+++ resources/views/public/partials/_menu.blade.php
@@ -10,5 +10,5 @@
 <div class="social-icons">
 	<a href="https://www.facebook.com/alsbeef" target="_blank"><i class="fa fa-facebook"></i></a>
 	<a href="https://twitter.com/alsitalianbeef" target="_blank"><i class="fa fa-twitter"></i></a>
-	<a href="#"><i class="fa fa-instagram"></i></a>
+	<a href="https://instagram.com/alsbeef/" target="_blank"><i class="fa fa-instagram"></i></a>
 </div>
